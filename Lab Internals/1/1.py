8369cf1b80e4243a3c89089e05bdad62b3cf1456
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-05-28 16:38:20 +0530

    Update 1.py

--- Lab Internals/1/1.py
+++ Lab Internals/1/1.py
@@ -67,18 +67,16 @@ print("Best First Search Path:", path)
 
 b)
 
+import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-# Generate data
-x = np.linspace(-5, 5, 100)
-y = np.linspace(-5, 5, 100)
-X, Y = np.meshgrid(x, y)
-Z = np.sin(np.sqrt(X**2 + Y**2))
+dataset = pd.read_csv('./ToyotaCorolla.csv')
+x = dataset['KM']
+y = dataset['Doors']
+z = dataset['Price']
 
-# Create 3D surface plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')  # Create a 3D subplot
-ax.plot_surface(X, Y, Z, cmap='viridis')  # Use ax.plot_surface for 3D surface plot
-ax.set_title('3D Surface Plot')
+ax = plt.axes(projection='3d')
+ax.plot_trisurf(x,y,z,cmap="jet")
+ax.set_title("3D Surface Plot")
+
 plt.show()
