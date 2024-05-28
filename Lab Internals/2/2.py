3450caa225fa000de12190a3d02dfc01ef6c0446
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-05-28 16:39:27 +0530

    Update 2.py

--- Lab Internals/2/2.py
+++ Lab Internals/2/2.py
@@ -80,16 +80,19 @@ print("A* Search Path:", path)
 
 b)
 
+# Using Toyota dataset
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 
-# Generate data
-x = np.linspace(-5, 5, 100)
-y = np.linspace(-5, 5, 100)
-X, Y = np.meshgrid(x, y)
-Z = np.sin(np.sqrt(X**2 + Y**2))
+dataset = pd.read_csv('./ToyotaCorolla.csv')
+x = dataset['KM']
+y = dataset['Weight']
+z = dataset['Price']
 
-# Create contour plot
-plt.contour(X, Y, Z, levels=20, cmap='RdGy')
+plt.tricontourf(x, y, z, levels=20, cmap='jet')
+plt.colorbar(label='Price')
+plt.xlabel('KM')
+plt.ylabel('Weight')
 plt.title('Contour Plot')
 plt.show()
